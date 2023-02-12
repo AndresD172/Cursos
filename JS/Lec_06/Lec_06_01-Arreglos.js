5efba182f8f8a193c4aaf9b799a9af85433816eb
Andrés Chacón <andreschaconmora3@gmail.com>
2023-02-11 22:27:57 -0600

    Se añadieron las primeras 2 clases de la lección 6 y se le dió formato a un archivo sin formato.

---
+++ JS/Lec_06/Lec_06_01-Arreglos.js
@@ -0,0 +1,16 @@
+// Un arreglo es un tipo object que admite el almacenamiento de múltuples valores.
+/**
+ * Hay dos maneras de definir un arreglo en Javascript, veamos cuáles son:
+ * 
+ * Manera #1:
+ * let array = new Array('val1','val2',...)
+ * 
+ * Manera #2:
+ * const array = ['val1','val2',...]
+ * */
+
+let array1 = new Array('val1','val2')
+console.log(array1)
+
+const array2 = ['val1','val2']
+console.log(array2)
---
+++ JS/Lec_06/Lec_06_02-Recorrido_de_arreglos.js
@@ -0,0 +1,28 @@
+/** 
+ * En Javascript, al igual que Python, los arrays guardan los elementos bajo un identificador de memoria o índice de elemento 
+ * dentro de la lista. Dicho identificador representa la posición del elemento dentro de la lista y, a diferencia de la numera
+ * ción tradicional, inicia contando los elementos desde el número 0.
+ * 
+ * Para acceder a los elementos de un array previamente definido, debemos saber nosotros la posición del elemento dentro de la
+ * lista y, seguidamente, usar la sintaxís 'array[i]', donde la variable i representa la posición del elemento. Esto nos retor
+ * nará el valor que se encuentra en dicha posición.
+ * */ 
+
+const array = ['Hola! Soy el valor alojado en la posición 0','Hola! Soy el valor alojado en la posición 1','Hola! Soy el valor alojado en la posición 2']
+
+console.log(array[0])
+console.log(array[1])
+console.log(array[2])
+
+/**
+ * Otra manera de recorrer los elementos de un array sin tener que especificar el valor del índice es usando el bucle for.
+ * Si recordamos de la sección pasada, el bucle for utiliza una variable para almacenar el valor que se usa en el contador
+ * condicional. Así que nosotros podemos aprovechar dicha variable y usar el valor que tome en cada iteración como si fuera
+ * el valor del índice del array que queremos consultar. 
+ */
+
+const newArray = ['Andrés','Andrea','Emma','Diego','Allan','Carlos','Dayra']
+
+for (let i=0; i<=6; i++) {
+    console.log(`Este es el elemento ${i}, el cual tiene el valor de ${newArray[i]}`)
+}
